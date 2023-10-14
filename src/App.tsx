c4275bbfaf3783a9cc2f1ff9752289ebcbba425d
cottonpup <younggyoung.lee1@gmail.com>
2023-10-14 19:17:29 +0900

    chore: 테일윈드, 데이지 UI 설정완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="rating gap-1">
+                <input
+                    type="radio"
+                    name="rating-3"
+                    className="mask mask-circle bg-red-900"
+                />
+                <input
+                    type="radio"
+                    name="rating-3"
+                    className="mask mask-heart bg-orange-400"
+                    checked
+                />
+                <input
+                    type="radio"
+                    name="rating-3"
+                    className="mask mask-heart bg-yellow-400"
+                />
+                <input
+                    type="radio"
+                    name="rating-3"
+                    className="mask mask-heart bg-lime-400"
+                />
+                <input
+                    type="radio"
+                    name="rating-3"
+                    className="mask mask-heart bg-green-400"
+                />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{tsx}'],
+    content: ['./src/**/*.{ts,tsx}'],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require('daisyui')],
 };
