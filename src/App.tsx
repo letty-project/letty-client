fdec11ee26f9e8633995205e7336135ad92487c3
cottonpup <younggyoung.lee1@gmail.com>
2023-10-17 01:13:33 +0900

    chore: svg 세팅완료

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,7 @@
 import './App.css';
 
 function App() {
-    // const [count, setCount] = useState(0);
-
-    return (
-        <>
-            <div className="rating gap-1">
-                <input
-                    type="radio"
-                    name="rating-3"
-                    className="mask mask-circle bg-red-900"
-                />
-                <input
-                    type="radio"
-                    name="rating-3"
-                    className="mask mask-heart bg-orange-400"
-                    checked
-                />
-                <input
-                    type="radio"
-                    name="rating-3"
-                    className="mask mask-heart bg-yellow-400"
-                />
-                <input
-                    type="radio"
-                    name="rating-3"
-                    className="mask mask-heart bg-lime-400"
-                />
-                <input
-                    type="radio"
-                    name="rating-3"
-                    className="mask mask-heart bg-green-400"
-                />
-            </div>
-        </>
-    );
+    return <div className="bg-gradient-to-r from-indigo-500 h-screen w-screen"></div>;
 }
 
 export default App;
