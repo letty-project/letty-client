befe3b1d6cd91d112f17357f8f8858e794d08baa
HamZik <b1y9mn@gmail.com>
2023-10-17 21:27:02 +0900

    feat: Home, Login 페이지로 라우팅 설정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
-      <div>로고</div>
-      <button>Login</button>
+      <Link to="/">
+        <div>로고</div>
+      </Link>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
     </header>
   );
 };
