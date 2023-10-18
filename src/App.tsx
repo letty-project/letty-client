1baf611c64ad9ebd765537553b43d8b0d8fa74d3
cottonpup <younggyoung.lee1@gmail.com>
2023-10-18 17:28:05 +0900

    feat: 배경 뒤 도트라인 추가하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 import './App.css';
+import DottedLine from './assets/dotted-line.svg';
 
 function App() {
     return (
-        <div className="h-screen w-screen bg-white bg-gradient-to-t from-[#15A09166] via-[#15A09166] to-white"></div>
+        <>
+            <div className="h-screen w-screen bg-white bg-gradient-to-t from-[#15A09166] via-[#15A09166] to-white flex justify-center items-center flex-col">
+                <img
+                    className="fixed left-4 bottom-0"
+                    src={DottedLine}
+                    alt="Dotted Line"
+                />
+            </div>
+        </>
     );
 }
 
