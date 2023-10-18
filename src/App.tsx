414776e46c67b3224b556cd6cb32b5498def887b
cottonpup <younggyoung.lee1@gmail.com>
2023-10-18 20:54:56 +0900

    style: 프리티어 컨벤션 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import './App.css';
-import { Card } from './components/Card';
-import { Header } from './components/Header';
-import DottedLine from './assets/dotted-line.svg';
+import "./App.css";
+import { Card } from "./components/Card";
+import { Header } from "./components/Header";
+import DottedLine from "./assets/dotted-line.svg";
 
 function App() {
-    return (
-        <>
-            <div className="h-screen w-screen bg-white bg-gradient-to-t from-[#15A09166] via-[#15A09166] to-white flex justify-center items-center flex-col">
-                <Header></Header>
-                <Card />
-                <img
-                    className="fixed left-4 bottom-0"
-                    src={DottedLine}
-                    alt="Dotted Line"
-                />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="h-screen w-screen bg-white bg-gradient-to-t from-[#15A09166] via-[#15A09166] to-white flex justify-center items-center flex-col">
+        <Header></Header>
+        <Card />
+        <img
+          className="fixed left-4 bottom-0"
+          src={DottedLine}
+          alt="Dotted Line"
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
