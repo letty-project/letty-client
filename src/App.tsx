a0722f8e634b6e7c762ee02deaf610b3a77ad2f4
cottonpup <younggyoung.lee1@gmail.com>
2023-10-26 04:00:09 +0900

    chore: 커밋충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
-import { Card } from "./components/Card";
-import Header from "./components/Header";
-import DottedLine from "./assets/dotted-line.svg";
+import './App.css';
+import { Card } from './components/Card';
+import Header from './components/Header1';
+import DottedLine from './assets/dotted-line.svg';
 
 function App() {
   return (
@@ -9,11 +9,7 @@ function App() {
       <div className="h-screen w-screen bg-white bg-gradient-to-t from-[#15A09166] via-[#15A09166] to-white flex justify-center items-center flex-col">
         <Header></Header>
         <Card />
-        <img
-          className="fixed left-4 bottom-0"
-          src={DottedLine}
-          alt="Dotted Line"
-        />
+        <img className="fixed left-4 bottom-0" src={DottedLine} alt="Dotted Line" />
       </div>
     </>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,17 @@
-// 람쥐님이 구현 예정
+import { Card } from '../components/Card';
+import { Header } from '../components/Header';
+import DottedLine from '../assets/dotted-line.svg';
 
-const Login = () => {
-  return <div>로그인 페이지</div>;
-};
+function Login() {
+  return (
+    <>
+      <div className="h-screen w-screen bg-white bg-gradient-to-t from-[#15A09166] via-[#15A09166] to-white flex justify-center items-center flex-col">
+        <Header></Header>
+        <Card />
+        <img className="fixed left-4 bottom-0" src={DottedLine} alt="Dotted Line" />
+      </div>
+    </>
+  );
+}
 
 export default Login;
