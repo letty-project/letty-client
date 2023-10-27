0d031a81804c8c283faf17bed741e995f065f6c2
cottonpup <67526014+cottonpup@users.noreply.github.com>
2023-10-27 18:11:07 +0900

    Revert "충돌된 헤더 컴포넌트 동일화, 조건에 따른 로그인 버튼 렌더링 (#19)"
    This reverts commit 26999463d3342d6c60cfaaff602cda5b06994f1f.

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "login",
-    element: <Login />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
-);
+)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "primary-default": "#15a091",
-        "primary-dark": "#116057",
-        "primary-light": "#75c7be",
-      },
+    content: ['./src/**/*.{ts,tsx}'],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [require("daisyui")],
+    plugins: [require('daisyui')],
 };
