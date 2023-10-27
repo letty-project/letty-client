f65fbb1e6c6847f42ae5c5cd7902f53ae3079827
HamZik <b1y9mn@gmail.com>
2023-10-27 15:44:29 +0900

    feat: router 설정

--- src/components/Header1.tsx
+++ src/components/Header1.tsx
@@ -1,24 +1,22 @@
-import { Link } from "react-router-dom";
-import Logo from "../assets/letty-icon.svg";
+import { Link } from 'react-router-dom';
+import Logo from '../assets/letty-icon.svg';
 
 const Header = () => {
   return (
     // <header className="h-28 w-screen fixed top-0"> 람쥐
-    <header className="flex flex-row justify-between px-40 py-5">
-      <Link to="/">
-        <div>
-          <img
-            className="fixed left-[125px] w-[150px] h-[125px]"
-            src={Logo}
-            alt="Letty Logo"
-          />
-        </div>
-      </Link>
-      <Link to="/login">
-        <button className="btn w-32 text-sm text-white bg-primary-default">
-          Login
-        </button>
-      </Link>
+    <header className="fixed top-0 left-0 w-screen p-5">
+      <div className="flex flex-row justify-between">
+        <Link to="/">
+          <div>
+            <img className="w-[150px]" src={Logo} alt="Letty Logo" />
+          </div>
+        </Link>
+        <Link to="/login">
+          <button className="btn w-32 text-sm text-white bg-primary-default">
+            Login
+          </button>
+        </Link>
+      </div>
     </header>
   );
 };
---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from './Header1';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <div className="h-screen w-screen bg-white bg-gradient-to-t from-[#15A09166] via-[#15A09166] to-white flex justify-center items-center flex-col">
+        <Header />
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
