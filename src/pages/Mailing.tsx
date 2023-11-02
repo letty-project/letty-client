52c6bb46799fcce05e69c975e5808f81c0cef8c9
biyam <b1y9mn@gmail.com>
2023-11-02 16:22:03 +0900

    feat: 작가 메일링 페이지 레이아웃 작성

--- src/pages/Mailing.tsx
+++ src/pages/Mailing.tsx
@@ -17,23 +17,27 @@ const Mailing = () => {
         </button>
       </div>
       <div>
-        <div className="w-[30rem] h-[10rem] bg-primary-default border ">
+        <div className="flex flex-col justify-around w-[30rem] h-[10rem] bg-primary-default border-0 ">
           <div className="text-white">현재 이 주소로 되어있어요</div>
-          <div className="flex flex-row">
-            <input />
-            <button className="btn bg-white text-primary-light my-[10px] border-0  font-semibold text-base w-full">
+          <div className="flex flex-row justify-between h-8">
+            <input className="w-3/4"/>
+            <button className="btn bg-white text-primary-light  border-0  font-semibold text-base ">
               완료
             </button>
           </div>
         </div>
-        <div className="flex flex-row justify-between">
-          <div>메일 쓰기</div>
-          <label htmlFor="send-all-button">전체 보내기</label>
-          <input id="send-all-button" type="checkbox" />
-        </div>
+        <div >
+          <div className="flex flex-row justify-between">
+            <div>메일 쓰기</div>
+            <div>
+              <input id="send-all-button" type="checkbox" />
+              <label htmlFor="send-all-button">전체 보내기</label>
+            </div>
+            </div>
+          </div>
         <div>
-          받는 사람 <input /> <button>주소록</button>
-          <button>보내기</button>
+          받는 사람 <input /> <button className="btn bg-white text-primary-light  border-0  font-semibold text-base ">주소록</button>
+          <button className="btn bg-primary-default text-primary-light  border-0  font-semibold text-base ">보내기</button>
         </div>
         <ReactQuill className="bg-white" theme="snow" />
       </div>
