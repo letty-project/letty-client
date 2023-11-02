d555b5616987fe2a6fec717d87911362b6eabbea
biyam <b1y9mn@gmail.com>
2023-11-02 13:02:46 +0900

    feat: 사이드바 버튼 추가
    - 메인 페이지 버튼
    - 구독자 관리 버튼

--- src/pages/Mailing.tsx
+++ src/pages/Mailing.tsx
@@ -1,12 +1,29 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import home from '../assets/home.svg';
+import person from '../assets/person.svg';
 
 const Mailing = () => {
   return (
-    <div>
-      <ReactQuill className="bg-white" theme="snow" />
+    <div className="flex flex-row">
+      <div className='flex flex-col'>
+        <button className="btn bg-[#15A091] my-[10px] border-0 text-white font-semibold text-base w-full">
+          <img
+          className=""
+          src={home}
+          alt="home"
+        />메인 페이지</button>
+        <button className="btn bg-[#15A091] my-[10px] border-0 text-white font-semibold text-base w-full"><img
+          className=""
+          src={person}
+          alt="person"
+        />구독자 관리</button>
+      </div>
+      <div>
+        <ReactQuill className="bg-white" theme="snow" />
+      </div>
     </div>
-  );
+    );
 };
 
 export default Mailing;
