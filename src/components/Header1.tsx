b108fb7b73409ca3e7693abe953e3f55504af5fd
biyam <b1y9mn@gmail.com>
2023-11-03 17:32:27 +0900

    feat: 헤더 로고 변경, 스타일링 변경

--- src/components/Header1.tsx
+++ src/components/Header1.tsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
-import Logo from '../assets/letty-icon.svg';
+import Logo from '../assets/letty.svg';
 
 const Header = () => {
   return (
     // <header className="h-28 w-screen fixed top-0"> 람쥐
-    <header className="w-screen h-24">
-      <div className="flex flex-row justify-between">
+    <header className="w-screen justify-center">
+      <div className="flex flex-row justify-between items-center h-24 mx-10">
         <Link to="/">
           <div>
-            <img className="w-[150px]" src={Logo} alt="Letty Logo" />
+            <img className="w-[100px]" src={Logo} alt="Letty Logo" />
           </div>
         </Link>
         <Link to="/login">
