8b1726becf00e1136c3638409777e6241c115680
biyam <b1y9mn@gmail.com>
2023-11-03 17:35:33 +0900

    feat: mailing page 구현
    - 레이아웃 설정
    - 스타일링
    - Icon 추가

--- src/pages/Mailing.tsx
+++ src/pages/Mailing.tsx
@@ -2,28 +2,40 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import home from '../assets/home.svg';
 import person from '../assets/person.svg';
+import CheckIcon from '../components/icon/CheckIcon';
+import AirplaneIcon from '../components/icon/AirplaneIcon';
 
 const Mailing = () => {
   return (
-    <div className="flex flex-row h-full bg-white">
-      <div className="h-full flex flex-col px-5 w-1/6  border-r-2">
-        <button className="btn bg-[#15A091] my-[10px] border-0 text-white font-semibold text-base w-full">
+    <div className="flex flex-row h-full w-screen bg-white">
+      <div className="h-full flex flex-col px-5 w-1/6  border-r-2 pt-12">
+        <button className="text-[16px] btn  h-[65px] bg-[#15A091] my-[10px] border-0 text-white font-semibold text-base">
           <img className="" src={home} alt="home" />
           메인 페이지
         </button>
-        <button className="btn bg-[#15A091] my-[10px] border-0 text-white font-semibold text-base w-full">
+        <button className="text-[16px] btn  h-[65px]  bg-[#15A091] my-[10px] border-0 text-white font-semibold text-base">
           <img className="" src={person} alt="person" />
           구독자 관리
         </button>
       </div>
-      <div className="flex justify-center items-center w-5/6 px-10">
-        <div className="flex flex-col w-full h-full">
-          <div>양수빈 님 안녕하세요!</div>
-          <div className="rounded-lg flex flex-col justify-around w-full h-[10rem] bg-primary-default border-0 ">
-            <div className="text-white">현재 이 주소로 되어있어요</div>
-            <div className="flex flex-row justify-between h-8">
+      <div className="flex justify-center items-center w-screen">
+        <div className="flex flex-col h-full w-[1100px]">
+          <div className="flex flex-row items-center">
+            <div className="font-black text-[30px]">양수빈 님 안녕하세요!</div>
+            <CheckIcon width="30px" height="30px" fill="#15A091" />
+          </div>
+          <div className="rounded-lg flex flex-col w-full p-7 bg-primary-default border-0 h-1/4 my-5">
+            <div className="flex flex-row">
+              <div className="text-white text-[28px] font-bold mr-3">
+                현재 이 주소로 되어있어요.
+              </div>
+              <div className="flex flex-row justify-center items-center">
+                <AirplaneIcon width="30px" height="30px" fill="white" />
+              </div>
+            </div>
+            <div className="flex flex-row justify-between pt-5">
               <input
-                className="w-3/4 rounded-3xl"
+                className="w-3/4 rounded-2xl h-11 px-5"
                 placeholder="https://y-oz6.tistory.com/"
               />
               <button className="btn bg-white text-primary-light  border-0  font-semibold text-base ">
@@ -32,29 +44,39 @@ const Mailing = () => {
             </div>
           </div>
           <div>
-            <div className="flex flex-row justify-between">
-              <div>메일 쓰기</div>
+            <div className="flex flex-row items-center justify-between ">
+              <div className=" text-primary-default text-[34px] font-black mr-3 my-3">
+                메일쓰기
+              </div>
               <div>
                 <input id="send-all-button" type="checkbox" />
                 <label htmlFor="send-all-button">전체 보내기</label>
               </div>
             </div>
           </div>
-          <div>
-            <label htmlFor="sender">받는 사람</label>
+          <div className="flex flex-row justify-between items-center">
+            <label className="text-[16px]" htmlFor="sender">
+              받는 사람
+            </label>
             <input
               id="sender"
-              className="rounded-3xl"
+              className="rounded-3xl border-2 border-[#15A091] w-2/3 h-10 px-5"
               placeholder="yangsubin@gmail.com"
             />
-            <button className="btn bg-white text-primary-light  border-0  font-semibold text-base ">
-              주소록
-            </button>
-            <button className="btn bg-primary-default text-primary-light  border-0  font-semibold text-base ">
-              보내기
-            </button>
+            <div className="flex flex-row">
+              <button className="btn text-[16px] bg-white text-primary-light  border-0  font-semibold text-base ">
+                주소록
+              </button>
+              <button className="btn text-[16px] bg-primary-default text-primary-light  border-0  font-semibold text-base ">
+                보내기
+              </button>
+            </div>
           </div>
-          <ReactQuill className="bg-white" theme="snow" />
+          <ReactQuill
+            className="bg-white my-2"
+            style={{ height: '200px' }}
+            theme="snow"
+          />
         </div>
       </div>
     </div>
