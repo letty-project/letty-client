13c5e3db85ba4fd8731b8941c18c7ad0b997ee86
biyam <b1y9mn@gmail.com>
2023-11-16 14:22:27 +0900

    feat: 기본 폰트 Pretendard 설정 (#29)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -8,11 +11,6 @@ export default {
         'primary-dark': '#116057',
         'primary-light': '#75c7be',
       },
-    },
-    fontFamily: {
-      sans: [
-        '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif',
-      ],
     },
   },
   daisyui: {
